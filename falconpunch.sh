ede3e9bb4892009b22b7c28a186bd207f2129d53
Bart Massey <bart@po8.org>
2016-04-17 15:56:27 -0700

    cleaned up falconpunch.sh

--- falconpunch.sh
+++ falconpunch.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 cd /home/fpunch/falconpunch
 case "$1" in
-    start)  python2 ./falconpunch.py ;;
-    stop)   PID=`cat falconpunch.pid` &&
-	    kill $PID ;;
+    start)
+	. ./challonge-api.sh
+	gunicorn -D -u fpunch -p ./falconpunch.pid -b 0.0.0.0:5256 \
+	  --log-file=./falconpunch.log
+	  falconpunch:app
+	;;
+    stop)
+	PID=`cat falconpunch.pid` &&
+	kill $PID
+	;;
 esac
